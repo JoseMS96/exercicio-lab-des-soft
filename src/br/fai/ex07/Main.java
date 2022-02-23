6125e6e6f3598c8e8d8a7ae52b35aeb1612ba118
Jose <melo.jeduardo@gmail.com>
2022-02-22 22:36:27 -0300

    adicionado ex07

--- src/br/fai/ex07/Main.java
+++ src/br/fai/ex07/Main.java
@@ -9,6 +9,23 @@ public class Main {
 	}
 	
 	private void start() {
+		calcular(5, 5);
+		calcular(10.27, 10.42);
+		calcular("44", " um texto qualquer");
+	}
 
+	private void calcular(int valor1, int valor2) {
+		int resultado = valor1 + valor2;
+		System.out.println("Resultado da operacao: " + resultado);
+	}
+	
+	private void calcular(double valor1, double valor2) {
+		double resultado = valor1 - valor2;
+		System.out.println("Resultado da operacao: " + resultado);		
+	}
+	
+	private void calcular(String valor1, String valor2) {
+		String resultado = valor1 + valor2;
+		System.out.println("Resultado da operacao: " + resultado);		
 	}
 }
